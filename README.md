27cab6d7c071ce1115061b499a21ed618feba859
DAGLaptop <dagutman@gmail.com>
2018-11-06 23:05:35 -0500

    working on webpack based superpixel viewer

---
+++ README.md
@@ -0,0 +1,6 @@
+This is code to help generate superpixels or tile based boundaries for large
+images stored in Girder.  In addition, there is a prototype UI that will support
+visualization of the boudnaries overlaid on the large images and allow some
+simple markup capabilities.
+
+
---
+++ webApp/src/index.js
@@ -0,0 +1,12 @@
+
+console.log('beginning webpack merge')
+
+
+// import {listFiles} from 'list-files-in-dir';
+
+// // As second parameter you can optionally pass the extension to filter the files
+// listFiles('./macroImages')
+//     .then(files => {
+//     	console.log(files)
+//         // do what ever you want with the file paths
+//     });
